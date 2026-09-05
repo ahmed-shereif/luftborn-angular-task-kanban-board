ccf6d4cc743b2b0df94ddfe87acafd19d6ca9b92
ahmed-shereif <ahmed1996sherif@gmail.com>
2026-09-05 03:24:10 +0300

    feat: update task statuses and timestamps in db.json; enhance analytics page styles and layout

--- src/app/features/dashboard/dashboard-page/dashboard-page.ts
+++ src/app/features/dashboard/dashboard-page/dashboard-page.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ActivityFeed } from '../components/activity-feed/activity-feed';
+import { AnalyticsPage } from '../../analytics/analytics-page/analytics-page';
 
 @Component({
   selector: 'app-dashboard-page',
   standalone: true,
-  imports: [ActivityFeed],
+  imports: [ActivityFeed, AnalyticsPage],
   templateUrl: './dashboard-page.html',
   styleUrl: './dashboard-page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
